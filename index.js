fbcfe5f307b8ef535143599d17e6d0a93e9ede23
Pablo Lopez <pablo_mtv@hotmail.com>
2020-08-25 15:12:22 -0300

    Servidor ya creado con Express

--- index.js
+++ index.js
@@ -23,9 +23,34 @@ fs.readFile("./texto.txt", function(err, data){
 
 //El modulo http me permite generar un servidor
 //Guardo la funcionalidad en una constante http 
-const http = require('http');
+//const http = require('http');
 
-http.createServer(function (req, res){
-    res.write('<h1>Hola mundo desde Node Js</h1>');
+//const colors = require("colors");
+
+//handleServer = manejador del servidor
+/*
+const handleServer = function (req, res){
+    res.writeHead(200, {"Content-type": "text/html"});
+    res.write('<h1>Hola Mundo</h1>');
     res.end();
-}).listen(3000);
+}
+
+const server = http.createServer(handleServer);
+server.listen(3000, function () {
+    console.log("Server on port 3000".yellow);
+});*/
+
+const express = require("express");
+
+const colors = require("colors");
+
+const server = express();
+
+server.get("/", function (req, res) {
+    res.send("<h1>Hola mundo con Express</h1>");
+    res.end;
+});
+
+server.listen(3000, function() {
+    console.log("Server on port 3000".red);
+});
